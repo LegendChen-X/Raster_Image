a034607dbff6be0d3a49aa2772763198de94d1ec
Xiang <chenxiang00825@gmail.com>
2020-05-09 14:31:25 -0400

    src/hsv_to_rgb.cpp done

--- src/hsv_to_rgb.cpp
+++ src/hsv_to_rgb.cpp
@@ -1,4 +1,5 @@
 #include "hsv_to_rgb.h"
+#include <stdlib.h>
 
 void hsv_to_rgb(
   const double h,
@@ -8,10 +9,57 @@ void hsv_to_rgb(
   double & g,
   double & b)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  r = 0;
-  g = 0;
-  b = 0;
-  ////////////////////////////////////////////////////////////////////////////
+    r = 0;
+    g = 0;
+    b = 0;
+    
+    double c = v * s;
+    double h_c = h / 60;
+    double x = c * (1 - abs(h_c % 2 - 1));
+    double m = v - c;
+    
+    double r_c;
+    double g_c;
+    double b_c;
+    
+    if(0<=h_c<=1)
+    {
+        r_c = c;
+        g_c = x;
+        b_c = 0;
+    }
+    else if(1<h_c<=2)
+    {
+        r_c = x;
+        g_c = c;
+        b_c = 0;
+    }
+    else if(2<h_c<=3)
+    {
+        r_c = 0;
+        g_c = c;
+        b_c = x;
+    }
+    else if(3<h_c<=4)
+    {
+        r_c = 0;
+        g_c = x;
+        b_c = c;
+    }
+    else if(4<h_c<=5)
+    {
+        r_c = x;
+        g_c = 0;
+        b_c = c;
+    }
+    else
+    {
+        r_c = c;
+        g_c = 0;
+        b_c = x;
+    }
+    
+    r = r_c + m;
+    g = g_c + m;
+    b = b_c + m;
 }
