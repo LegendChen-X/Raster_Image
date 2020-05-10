5a216ca7519e41c941704c723cba66cbe2de9cb0
Xiang <chenxiang00825@gmail.com>
2020-05-10 10:46:39 -0400

    All part done only for debug demosic

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
   std::vector<unsigned char> desaturated;
   desaturate(rgb,width,height,0.25,desaturated);
   write_ppm("desaturated.ppm",desaturated,width,height,3);
-/*
+
   // Alpha composite multiple images (if present)
   std::vector<unsigned char> composite_rgba;
   read_rgba_from_png(input_filenames[0],composite_rgba,width,height);
@@ -96,5 +96,4 @@ int main(int argc, char *argv[])
   std::vector<unsigned char> composite;
   rgba_to_rgb(composite_rgba,width,height,composite);
   write_ppm("composite.ppm",composite,width,height,3);
- */
 }
--- src/over.cpp
+++ src/over.cpp
@@ -1,4 +1,11 @@
 #include "over.h"
+typedef struct Pixel
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    double alpha;
+}pixel;
 
 void over(
   const std::vector<unsigned char> & A,
@@ -7,8 +14,48 @@ void over(
   const int & height,
   std::vector<unsigned char> & C)
 {
-  C.resize(A.size());
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+    C.resize(A.size());
+    
+    pixel A_buff[height][width];
+    pixel B_buff[height][width];
+    pixel C_buff[height][width];
+    
+    int index = 0;
+    for(int i=0;i<height && index<height*width*4;++i)
+    {
+        for(int j=0;j<width && index<height*width*4;++j)
+        {
+            A_buff[i][j].r = A[index];
+            B_buff[i][j].r = B[index++];
+            A_buff[i][j].g = A[index];
+            B_buff[i][j].g = B[index++];
+            A_buff[i][j].b = A[index];
+            B_buff[i][j].b = B[index++];
+            A_buff[i][j].alpha = (double)A[index] / 255;
+            B_buff[i][j].alpha = (double)B[index++] / 255;
+        }
+    }
+    
+    for(int i=0;i<height;++i)
+    {
+        for(int j=0;j<width;++j)
+        {
+            C_buff[i][j].r = A_buff[i][j].r * A_buff[i][j].alpha + B_buff[i][j].r * (1 - A_buff[i][j].alpha);
+            C_buff[i][j].g = A_buff[i][j].g * A_buff[i][j].alpha + B_buff[i][j].g * (1 - A_buff[i][j].alpha);
+            C_buff[i][j].b = A_buff[i][j].b * A_buff[i][j].alpha + B_buff[i][j].b * (1 - A_buff[i][j].alpha);
+            C_buff[i][j].alpha = A_buff[i][j].alpha * A_buff[i][j].alpha + B_buff[i][j].alpha * (1 - A_buff[i][j].alpha);
+        }
+    }
+    
+    index = 0;
+    for(int i=0;i<height && index<height*width*4;++i)
+    {
+        for(int j=0;j<width && index<height*width*4;++j)
+        {
+            C[index++] = C_buff[i][j].r;
+            C[index++] = C_buff[i][j].g;
+            C[index++] = C_buff[i][j].b;
+            C[index++] = C_buff[i][j].alpha;
+        }
+    }
 }
