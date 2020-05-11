e78992ff8505ce6044fae65239e0f867f32aa179
Xiang <chenxiang00825@gmail.com>
2020-05-10 20:27:06 -0400

    output

---
+++ build/CMakeFiles/raster.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/chen_xiang/Desktop/CSC418_a1/main.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/main.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/demosaic.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/demosaic.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/desaturate.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/desaturate.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/hsv_to_rgb.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/hsv_to_rgb.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/hue_shift.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/hue_shift.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/over.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/over.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/reflect.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/reflect.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/rgb_to_gray.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/rgb_to_gray.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/rgb_to_hsv.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/rgb_to_hsv.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/rgba_to_rgb.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/rgba_to_rgb.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/rotate.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/rotate.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/simulate_bayer_mosaic.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/simulate_bayer_mosaic.cpp.o"
+  "/Users/chen_xiang/Desktop/CSC418_a1/src/write_ppm.cpp" "/Users/chen_xiang/Desktop/CSC418_a1/build/CMakeFiles/raster.dir/src/write_ppm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../json"
+  "../shared/json"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
